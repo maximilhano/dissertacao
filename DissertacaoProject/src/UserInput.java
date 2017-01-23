4c3ba4050ac84140cea8e0167d6e89347ed72b61
maximilhano <mvk@ua.pt>
2017-01-23 15:21:15 +0000

    message

--- DissertacaoProject/src/UserInput.java
+++ DissertacaoProject/src/UserInput.java
@@ -1,3 +1,11 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,13 +17,20 @@
  * @author maksym
  */
 public class UserInput {
-    private String input;
+    private String userQuery;
+    private BufferedReader input;
 
-    public String getInput() {
-        return this.input;
+    public String getUserQuery() {
+        return this.userQuery;
     }
-
-    public void setInput(String input) {
-        this.input = input;
+    
+    public void requestUserInput(){
+        try {
+            System.out.println("Waiting for user input: ");
+            this.input = new BufferedReader(new InputStreamReader(System.in, "utf-8"));
+            this.userQuery = this.input.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(UserInput.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
