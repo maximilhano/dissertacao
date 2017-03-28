6233ba59d1054e55fb3ed983ccd00acc3a24d05d
maximilhano <mvk@ua.pt>
2017-03-28 11:51:52 +0100

    questionAnalysis answerAnalysis questionsEnum semantic module

--- DissertacaoProject/src/Database/LocalDatabase.java
+++ DissertacaoProject/src/Database/LocalDatabase.java
@@ -10,5 +10,23 @@ package Database;
  * @author maksym
  */
 public class LocalDatabase {
+
+    private final String DB = "http://localhost:3030/default/";
+    private final String dbpediaEndpoint = "http://dbpedia.org/sparql/";
+    private final String fusekiQuery = DB + "query";
+    private final String fusekiUpdate = DB + "update";
+
+    private final String PREFIX = "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n"
+            + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
+            + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+            + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+            + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
+            + "PREFIX dc: <http://purl.org/dc/elements/1.1/>\n"
+            + "PREFIX : <http://dbpedia.org/resource/>\n"
+            + "PREFIX dbpedia2: <http://dbpedia.org/property/>\n"
+            + "PREFIX dbpedia: <http://dbpedia.org/>\n"
+            + "PREFIX dbo: <http://dbpedia.org/ontology/>\n"
+            + "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n";
+    
     
 }
